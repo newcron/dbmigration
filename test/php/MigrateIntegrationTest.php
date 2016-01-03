280b11cca569b78252a5729d34c2e329c88d02b5
Matthias Huttar <mhuttar@olx.com>
2016-01-03 23:03:42 +0100

    test to run migrations incrementally

--- test/php/MigrateIntegrationTest.php
+++ test/php/MigrateIntegrationTest.php
@@ -23,4 +23,20 @@ class MigrateTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("v3_test.sql", $migrations[0]->getFilename());
         $this->assertEquals("v10_test.sql", $migrations[1]->getFilename());
     }
+
+    public function testInitializeIncremental()
+    {
+
+        $pdo = $this->aNewInitializedSchema();
+
+        (new Migrate($pdo, new \SplFileInfo(__DIR__ . "/../testsets/incremental/step1")))->runMissingMigrations();
+        (new Migrate($pdo, new \SplFileInfo(__DIR__ . "/../testsets/incremental/step2")))->runMissingMigrations();
+
+        $migrations = (new LoadMigrations($pdo))->allInstalledMigrations();
+        $this->assertEquals("v3_test.sql", $migrations[0]->getFilename());
+        $this->assertEquals("v10_test.sql", $migrations[1]->getFilename());
+        $this->assertEquals("v13_test.sql", $migrations[2]->getFilename());
+    }
+
+
 }
